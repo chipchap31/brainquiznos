1bf4a426a1daece8748101992e8a3cbc63b80a86
jomari <jomarialang.com>
2019-12-03 20:14:17 +0000

    feat: finished signup form

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page. */
+router.post("/signup", function(req, res, next) {
+  res.send(req.body);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require("express");
+let app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "assets")));
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.listen(4000, () => console.log("Example app listening on port 4000!"));
