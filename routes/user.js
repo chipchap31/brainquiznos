76ed6a3a45e14d18e15d95a30d0921e42dce9609
jomari <jomarialang.com>
2019-12-05 11:56:14 +0000

    feat: login receiving success

--- routes/user.js
+++ routes/user.js
@@ -56,7 +56,38 @@ router.post("/signup", async function(req, res, next) {
   }
 });
 
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const USERS = mongoose.model("users");
+
+  const user = await USERS.findOne({ username });
+
+  const match = user ? await bcrypt.compare(password, user.password) : false;
+
+  // no user find with the given
+  // username is found
+  if (!user) {
+    return res.render("login", {
+      user: false,
+      err: true,
+      errMessage: {
+        username: "Account does not exist.",
+        password: false
+      }
+    });
+  } else if (!match) {
+    return res.render("login", {
+      user: false,
+      err: true,
+      errMessage: {
+        username: false,
+        password: "Password does not match."
+      }
+    });
+  } else {
+    res.send({ message: "success" });
+  }
+});
 
 module.exports = router;
 
