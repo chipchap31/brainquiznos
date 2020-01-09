81622f30def0840fb397230dc8d8fd7a3295038e
jomari <jomarialang.com>
2020-01-09 17:27:58 +0000

    first testing in production

--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-const secondsToAdd = 1 * 20 * 1000;
+const secondsToAdd = 3 * 60 * 1000;
 router.get("/fetch", requireLogin, async (req, res) => {
   const GAMES = mongoose.model("games");
   const id = req.session.user.id;
@@ -16,20 +16,25 @@ router.post("/new", requireLogin, async (req, res) => {
 
   const id = req.session.user.id;
   let replenishDate;
+  // find lost games that life is replenishing
   const lostGames = await GAME.find({
     _user: id
   }).$where(function() {
     return this.replenishDate > Date.now();
   });
+  // check if the latest game finished replenishing its life
+
   replenishDate = new Date(Date.now() + secondsToAdd);
+
   if (lostGames.length > 0) {
     // if there are lost games
-    const latestGame = await GAME.find({ _user: req.session.user.id }).sort({
+    const latestGame = await GAME.findOne({ _user: req.session.user.id }).sort({
       datePlayed: -1
     });
-
-    replenishDate = latestGame[0].replenishDate.getTime() + secondsToAdd;
+    console.log(latestGame);
+    replenishDate = latestGame.replenishDate.getTime() + secondsToAdd;
   }
+
   const { mode } = req.body;
   try {
     const newGame = await new GAME({
@@ -74,24 +79,24 @@ router.post("/update", async (req, res) => {
 
     if (lostGames.length > 0) {
       // if there are lost games
-      const latestGame = await GAME.find({ _user: req.session.user.id }).sort({
+      const latestGame = await GAME.findOne({ _user: id }).sort({
         datePlayed: -1
       });
 
-      replenishDate = lostGames[0].replenishDate.getTime();
+      replenishDate = latestGame.replenishDate.getTime();
     }
 
     const findGame = await GAME.findById(req.body.id);
-    console.log(findGame);
+
     await findGame.updateOne({
-      points,
+      points: won ? points : 0,
       clicks,
       won,
 
       replenishDate: won ? Date.now() : replenishDate
     });
     const updatedGame = await GAME.findById(req.body.id);
-    console.log(updatedGame);
+
     res.send({ date: updatedGame.replenishDate });
   } catch (e) {
     throw new Error(e);
